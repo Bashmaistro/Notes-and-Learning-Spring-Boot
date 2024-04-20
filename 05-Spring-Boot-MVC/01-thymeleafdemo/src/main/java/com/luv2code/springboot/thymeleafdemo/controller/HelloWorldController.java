edfa94acf615b66eaff6d144fd4920473c005ceb
Bashmaistro <emirhanyildiz@ogr.eskisehir.edu.tr>
2024-04-20 05:10:34 +0300

    thymleaf

---
+++ 05-Spring-Boot-MVC/01-thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,47 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+
+    //new a controller emthod show initial html form
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+
+
+    //need a contoler method to process the html form
+    @RequestMapping ("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping ("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        //read the request parameter from the html form
+        String theName = request.getParameter("studentName");
+
+        //convert the data to all caps
+        theName = theName.toUpperCase();
+
+        //create the message
+        String result = "Yo!" + theName;
+
+        //add to the model
+        model.addAttribute("message" , result);
+
+        return "helloworld";
+
+
+
+    }
+}
