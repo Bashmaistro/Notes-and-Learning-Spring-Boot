94e7696a95507dc5e5f8bd37d384b355d3759078
Bashmaistro <emirhanyildiz@ogr.eskisehir.edu.tr>
2024-04-21 20:41:31 +0300

    check box and radio button exercise

---
+++ 05-Spring-Boot-MVC/01-thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -0,0 +1,60 @@
+package com.luv2code.springboot.thymeleafdemo.model;
+
+import java.util.List;
+
+public class Student {
+
+    private String firstName;
+    private  String lastName;
+
+    private String country;
+
+    private String favoriteLang;
+
+    private List<String> favoriteSystem;
+
+    public Student(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLang() {
+        return favoriteLang;
+    }
+
+    public void setFavoriteLang(String favoriteLang) {
+        this.favoriteLang = favoriteLang;
+    }
+
+    public List<String> getFavoriteSystem() {
+        return favoriteSystem;
+    }
+
+    public void setFavoriteSystem(List<String> favoriteSystem) {
+        this.favoriteSystem = favoriteSystem;
+    }
+}
---
+++ Notes/Thymleaf.md
@@ -0,0 +1,14 @@
+
+
+![](Attachment/Pasted%20image%2020240421014554.png)
+
+
+As you can see the html code above here when the submit action finished apply the get method the ProcessFormVersionThree method will called  and the studentName pushed to the method.
+
+
+![](Attachment/Pasted%20image%2020240421015703.png)
+
+And the our method will take the studentName parameter and return the helloworld.html file. And pushed into  a model.
+
+![](Attachment/Pasted%20image%2020240421015838.png)
+Our html file take the message and parameter as a studentName from our method.
